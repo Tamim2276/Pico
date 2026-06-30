988b6cee202d5d448d9e972f79c5837475e0d5d7
Tamim2276 <tamim2276007@gmail.com>
2026-07-01 03:15:45 +0600

    feat: initial project setup with SDK 54, TypeScript, navigation, Firebase, and folder structure

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Pico is running! ✅</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            "@domain": "./src/domain",
+            "@data": "./src/data",
+            "@presentation": "./src/presentation",
+            "@di": "./src/di",
+            "@shared": "./src/shared",
+          },
+        },
+      ],
+    ],
+  };
+};
